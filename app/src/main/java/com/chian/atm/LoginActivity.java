3b6702a1a1c52f0fdca62d90fa68453f6416a967
sungchian <bnbn870829@gmail.com>
2017-11-28 11:56:03 +0800

    finish

--- app/src/main/java/com/chian/atm/LoginActivity.java
+++ app/src/main/java/com/chian/atm/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.chian.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,7 +15,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void login (View view){
-
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        EditText edPasswd = (EditText) findViewById(R.id.passwd);
+        String userid = edUserid.getText().toString();
+        String passwd = edPasswd.getText().toString();
+        if ("jack".equals(userid) && "1234".equals(passwd)){
+            Toast.makeText(this , "登入成功" , Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("登入")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK" , null)
+                    .show();
+        }
     }
     public void cancel(View view){
 
